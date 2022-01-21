38b12a52d930029891ece55a9ee7142849af26f3
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-21 16:51:22 -0300

    Camada de serviços, registro de componentes

---
+++ src/main/java/com/project/code1/services/UserService.java
@@ -0,0 +1,25 @@
+package com.project.code1.services;
+
+import com.project.code1.entities.User;
+import com.project.code1.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
