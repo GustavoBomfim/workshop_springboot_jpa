a4e5c7a8ec8f64f27f4b8f826d2503c673fcb698
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-21 15:25:05 -0300

    H2 database, teste profile

--- src/main/java/com/project/code1/entities/User.java
+++ src/main/java/com/project/code1/entities/User.java
@@ -1,11 +1,16 @@
 package com.project.code1.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
