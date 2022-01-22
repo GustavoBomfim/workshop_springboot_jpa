f7173a7344d90f8893088f995f53a480d83be900
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-22 13:25:13 -0300

    Enum OrderStatus

--- src/main/java/com/project/code1/entities/Order.java
+++ src/main/java/com/project/code1/entities/Order.java
@@ -1,6 +1,7 @@
 package com.project.code1.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.code1.config.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,6 +20,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
 
     public Order(){
     }
@@ -28,9 +30,10 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,6 +51,24 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
     }
 
     @Override
