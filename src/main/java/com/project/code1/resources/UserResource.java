156811e8d712d153ddb1a1f374055b63782a29e4
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-23 12:39:05 -0300

    User update

--- src/main/java/com/project/code1/resources/UserResource.java
+++ src/main/java/com/project/code1/resources/UserResource.java
@@ -43,7 +43,12 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
