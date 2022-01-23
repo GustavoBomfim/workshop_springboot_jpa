71badca716ea3a9dff4bd7675d3e530fd398db55
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-23 11:57:45 -0300

    SubTotal e Total metodos

--- src/main/java/com/project/code1/entities/Order.java
+++ src/main/java/com/project/code1/entities/Order.java
@@ -29,6 +29,8 @@ public class Order implements Serializable {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
+
+
     public Order(){
     }
 
@@ -90,6 +92,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems(){
         return items;
+    }
+
+    public Double getTotal(){
+        double sum = 0.0;
+        for(OrderItem x : items){
+            sum = sum + x.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
--- src/main/java/com/project/code1/entities/Payment.java
+++ src/main/java/com/project/code1/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.project.code1.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -15,6 +17,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
